20d5a30d0ff44ff54635b3ba62493e1ce193af2c
adams <108461931+AdamsFontil@users.noreply.github.com>
2026-02-23 17:31:44 -0500

    clicking on pokemon reveal their stats

--- e2e-tests/pokemon_playwright_e2e.spec.js
+++ e2e-tests/pokemon_playwright_e2e.spec.js
@@ -5,5 +5,11 @@ describe('Pokedex', () => {
     await page.goto('')
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  }),
+  test('clicking on pokemon(butterfree) works', async ({ page }) => {
+    await page.goto('')
+    await page.getByText('butterfree').click()
+    await expect(page.getByText('tinted lens')).toBeVisible()
+    await expect(page.getByText('compound eyes')).toBeVisible()
   })
 })

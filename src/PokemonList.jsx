7f9130e4658d5df6557ec83360e957c5fb00df48
adams <108461931+AdamsFontil@users.noreply.github.com>
2026-02-23 20:26:46 -0500

    linting complete

--- src/PokemonList.jsx
+++ src/PokemonList.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 const PokemonList = ({ pokemonList }) => {
   return (
@@ -20,7 +20,7 @@ const PokemonList = ({ pokemonList }) => {
         ))}
       </div>
     </>
-  );
-};
+  )
+}
 
-export default PokemonList;
+export default PokemonList
